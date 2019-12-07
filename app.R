24093d900583504b02dc97c637ececbb96a8cf4d
Ryan Homer <ryan@murage.ca>
2019-12-07 13:14:28 -0800

    Add dataset and filter panels with filter controls populated with default values

--- app.R
+++ app.R
@@ -7,22 +7,86 @@ library(plotly)
 # Load assets
 app <- Dash$new(assets_folder = "assets")
 
+movies_df <- read_csv("data/movies.csv")
+
+make_checklist_options <- function(values) {
+  values %>%
+    map(~ (list(label = ., value = .)))
+}
+
+genre_options <- movies_df %>%
+  select("Major_Genre") %>%
+  na.omit() %>%
+  unique() %>%
+  pull("Major_Genre") %>%
+  sort() %>%
+  make_checklist_options()
+
+ratings_options <- c("G", "PG", "PG-13", "R", "NC-17") %>%
+  make_checklist_options()
+
+year_options <- movies_df %>%
+  select("Release_Year") %>%
+  na.omit() %>%
+  unique() %>%
+  pull("Release_Year") %>%
+  sort() %>%
+  make_checklist_options()
+
+#
+# Default values for filters
+#
+default_genres = c("Action", "Adventure", "Comedy", "Drama", "Horror", "Romantic Comedy", "Thriller/Suspense")
+default_ratings = c("PG", "PG-13", "R")
+
 app$layout(
   htmlDiv(list(
     htmlDiv(list(
       htmlDiv(list(
         htmlDiv(list(
-          htmlP("Genres", className="app-main--container-title")
+          htmlP("Genres", className="app-main--container-title"),
+          dccChecklist(id="cb-genres",
+                       className="app-main--genre-cb-container",
+                       inputClassName="app-main--cb-input",
+                       labelClassName="app-main--cb-label",
+                       options = genre_options,
+                       value = default_genres)
         ), className = "app-main--genre-container app-main--filter-panel"),
         htmlDiv(list(
-          htmlP('test')
+          htmlP("MPAA Ratings", className="app-main--container-title"),
+          dccChecklist(
+            id="cb-ratings",
+            className="app-main--rating-cb-container",
+            inputClassName="app-main--cb-input",
+            labelClassName="app-main--cb-label",
+            options = ratings_options,
+            value = default_ratings
+          )
         ), className = "app-main--rating-container app-main--filter-panel"),
         htmlDiv(list(
-          htmlP('test')
+          htmlP("Release Year", className="app-main--container-title"),
+          htmlDiv(list(
+            htmlDiv(list(
+              htmlP("from", className="app-main--dropdown-title"),
+              dccDropdown(id="dd-year-from",
+                          options = year_options,
+                          value = (year_options %>% first())$label,
+                          className="app-main--dropdown",
+                          clearable = FALSE)
+            ), className="app-main--dropdown-wrapper"),
+            htmlDiv(list(
+              htmlP("to", className="app-main--dropdown-title"),
+              dccDropdown(id="dd-year-to",
+                          options = year_options,
+                          value = (year_options %>% last())$label,
+                          className="app-main--dropdown",
+                          clearable = FALSE)
+            ), className="app-main--dropdown-wrapper")
+          ), className="app-main--year-selector")
         ), className = "app-main--year-container app-main--filter-panel")
       ), className = "app-main--panel-left"),
       htmlDiv(list(
-        htmlP('test')
+        htmlP('charts go here')
       ), className = "app-main--panel-right")
     ), className = "app-main--container")
   ), className = "wrapper")
