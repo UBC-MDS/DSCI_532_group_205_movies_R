91e4c9d19e945e4e5c003690703aaaa566a5feb6
Furqan Khan <fkhan72@uwo.ca>
2019-12-13 02:34:59 -0800

    chart 1 fully working

--- src/chart_1.R
+++ src/chart_1.R
@@ -13,11 +13,17 @@ create_chart_1 <- function(df) {
   #   layout(title = "Click on a line to highlight a year") %>%
   #   highlight(dynamic = TRUE, selectize = TRUE)
 
-  p <- df %>%
-    group_by(Release_Year) %>%
-    summarize(US_Gross = sum(US_Gross) / 1e6) %>%
-    ggplot(aes(x = Release_Year, y = US_Gross)) +
-    geom_bar(stat = "identity")
+  top_us_gross_df <- df %>%
+    arrange(desc(US_Gross)) %>%
+    head(10)
+
+  p <- ggplot(top_us_gross_df,aes(x = reorder(Title, US_Gross), y = US_Gross/1e6)) +
+     geom_bar(stat = "identity",  show.legend = FALSE, fill="chocolate4") +
+     coord_flip() +
+     labs(x = "",
+           y = "Gross Revenue (millions of USD)", 
+           title = "Highest Grossing US Movies") +
+     theme_bw()
 
   ggplotly(p)
 }
