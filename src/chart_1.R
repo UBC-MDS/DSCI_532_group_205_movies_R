8130abdb103fea153baf95c3ef9b9946b524e7b2
Ryan Homer <ryan@murage.ca>
2019-12-13 22:14:15 -0800

    Update colour and customize tooltips in chart 1

--- src/chart_1.R
+++ src/chart_1.R
@@ -1,30 +1,22 @@
 create_chart_1 <- function(df) {
-  # df <- movies_df %>%
-  #   group_by(Release_Year) %>%
-  #   summarize(US_Gross = sum(US_Gross) / 1e6)
-  #
-  # sd <- highlight_key(df, ~Release_Year, "Select a year")
-  #
-  # p <- sd %>%
-  #   ggplot(aes(x = Release_Year, y = US_Gross, group = Release_Year)) +
-  #   geom_bar(stat = "identity")
-  #
-  # ggplotly(p, tooltip = "Release_Year") %>%
-  #   layout(title = "Click on a line to highlight a year") %>%
-  #   highlight(dynamic = TRUE, selectize = TRUE)
-
   top_us_gross_df <- df %>%
     arrange(desc(US_Gross)) %>%
     head(10)
 
-  p <- ggplot(top_us_gross_df,aes(x = reorder(Title, US_Gross), y = US_Gross/1e6)) +
-     geom_bar(stat = "identity",  show.legend = FALSE, fill="chocolate4") +
+  p <- top_us_gross_df %>%
+     ggplot(aes(x = reorder(Title, US_Gross),
+                y = US_Gross_per_million,
+                text = paste0("Year: ", Release_Year,
+                              "<br />Title: ", Title,
+                              "<br />MPAA Rating: ", MPAA_Rating,
+                              "<br />US Gross (per million): ", US_Gross_per_million))) +
+     geom_bar(stat = "identity",  show.legend = FALSE, fill="#396362") +
      coord_flip() +
      labs(x = "",
-           y = "Gross Revenue (millions of USD)", 
+           y = "Gross Revenue (millions of USD)",
            title = "Highest Grossing US Movies") +
      theme_bw()
 
-  ggplotly(p) %>%
+  ggplotly(p, tooltip = c("text")) %>%
     layout(clickmode = 'event+select')
 }
